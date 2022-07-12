eadbf2a3f87db60fa0cb66de76d3bf7ee8028b4e
bcbrockway <bobby.brockway@gmail.com>
2022-07-12 10:39:22 +0100

    add metrics (#2)

--- pkg/common/common.go
+++ pkg/common/common.go
@@ -1,6 +1,22 @@
 package common
 
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
 type CollectedData struct {
 	Summary     string
 	Description string
 }
+
+var (
+	PromErrors = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "atlassian_automator_errors_total",
+			Help: "The number of errors encountered by the main package",
+		},
+		[]string{
+			"package",
+		})
+)

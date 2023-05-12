e5b07c7747c4e0f19c48ecac0cb04beeff3ed785
Bobby Brockway <bobby.brockway@gmail.com>
2023-05-12 12:55:14 +0100

    return the correct data

--- pkg/lastupdate/lastupdate.go
+++ pkg/lastupdate/lastupdate.go
@@ -3,6 +3,10 @@ package lastupdate
 import (
 	"context"
 	"fmt"
+	"log"
+	"path"
+	"sort"
+	"time"
 
 	"github.com/mintel/atlassian-automator/pkg/common"
 	"github.com/mintel/atlassian-automator/pkg/confluence"
@@ -10,9 +14,9 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
-// const (
-// 	pkg string = "lastupdate"
-// )
+const (
+	pkg string = "lastupdate"
+)
 
 var (
 	PromLastUpdatePagesTotal = promauto.NewGaugeVec(
@@ -20,7 +24,7 @@ var (
 			Name: "atlassian_automator_lastupdate_pages_total",
 			Help: "The number of pages monitored by lastupdate jobs",
 		},
-		[]string{"name"},
+		[]string{"job_name"},
 	)
 )
 
@@ -31,111 +35,6 @@ type Config struct {
 	Type         string `yaml:"type"`
 	ResultsLimit int    `yaml:"resultsLimit"`
 }
-
-// findChildren takes a confluence.Pages object and iterates through it to find child pages of the provided
-// parentPageID, then calls itself to find children of those children and so on.
-func findChildren(allPages confluence.Pages, parentPageID string) confluence.Pages {
-	var children confluence.Pages
-
-	for _, page := range allPages.Results {
-		if page.ParentID == parentPageID {
-			children.Results = append(children.Results, page)
-		}
-	}
-
-	if len(children.Results) > 0 {
-		for _, page := range children.Results {
-			next := findChildren(allPages, page.ID)
-			children.Results = append(children.Results, next.Results...)
-		}
-	}
-	return children
-}
-
-// func filterResults(ancestorID string, olderThan time.Duration, allPages confluence.Pages) error {
-
-// 	var filtered confluence.Pages
-
-// 	for _, result := range allPages.Results {
-
-// 		// Filter out anything not under the specified page
-// 		wanted := false
-// 		for _, ancestor := range result. {
-// 			if ancestor.ID == ancestorID {
-// 				wanted = true
-// 				break
-// 			}
-// 		}
-
-// 		// Filter out anything not older than the provided date
-// 		if wanted {
-// 			layout := "2006-01-02T15:04:05.000Z"
-// 			lastUpdatedTime, err := time.Parse(layout, result.History.LastUpdated.When)
-// 			if err != nil {
-// 				common.PromErrors.WithLabelValues(pkg).Inc()
-// 				return nil, err
-// 			}
-// 			oldestAllowedTime := time.Now().Add(-olderThan)
-// 			if !lastUpdatedTime.Before(oldestAllowedTime) {
-// 				wanted = false
-// 			}
-// 		}
-
-// 		if wanted {
-// 			filtered = append(filtered, result)
-// 		}
-// 	}
-// 	return filtered, nil
-// }
-
-// func Run(jobName string, api goconfluence.API, cfg Config, baseURL *url.URL) ([]common.CollectedData, error) {
-
-// 	var collectedData []common.CollectedData
-
-// 	// Get content by query
-// 	res, err := api.GetContent(goconfluence.ContentQuery{
-// 		SpaceKey: cfg.SpaceKey,
-// 		Expand:   []string{"history.lastUpdated", "ancestors"},
-// 		Type:     cfg.Type,
-// 		Limit:    cfg.ResultsLimit,
-// 		OrderBy:  "history.lastUpdated.when desc",
-// 	})
-// 	if err != nil {
-// 		common.PromErrors.WithLabelValues(pkg).Inc()
-// 		log.Printf("%s: error getting content from confluence: %s", jobName, err)
-// 		return nil, err
-// 	}
-
-// 	duration, err := time.ParseDuration(cfg.Duration)
-// 	if err != nil {
-// 		common.PromErrors.WithLabelValues(pkg).Inc()
-// 		log.Printf("%s: couldn't parse duration %s: %s", jobName, cfg.Duration, err)
-// 		return nil, err
-// 	}
-
-// 	allPages, err := filterResults(cfg.ParentPageID, duration, res)
-// 	if err != nil {
-// 		common.PromErrors.WithLabelValues(pkg).Inc()
-// 		log.Printf("%s: couldn't filter results: %s", jobName, err)
-// 		return nil, err
-// 	}
-// 	log.Printf("%s: %+v pages found older than %s", jobName, len(allPages), cfg.Duration)
-// 	sort.SliceStable(allPages, func(i, j int) bool {
-// 		return allPages[i].History.LastUpdated.When > allPages[j].History.LastUpdated.When
-// 	})
-
-// 	for _, page := range allPages {
-// 		pageURL := *baseURL
-// 		pageURL.Path = path.Join(pageURL.Path, page.Links.TinyUI)
-// 		collectedData = append(collectedData,
-// 			common.CollectedData{
-// 				Summary:     fmt.Sprintf("\"%s\" has not been updated since %s", page.Title, page.History.LastUpdated.When),
-// 				Description: fmt.Sprintf("Page ID: %s\nPage Title: %s\nLast Updated: %s\nURL: %s\n\nPlease review this page and update any out-of-date information if required.\n\n", page.ID, page.Title, page.History.LastUpdated.When, pageURL.String()),
-// 			},
-// 		)
-// 	}
-// 	return collectedData, nil
-// }
 
 // getSpaceIDFromKey gets the ID of a space using the provided key
 func getSpaceIDFromKey(ctx context.Context, key string) (string, error) {
@@ -156,6 +55,43 @@ func getSpaceIDFromKey(ctx context.Context, key string) (string, error) {
 	return spaces.Results[0].ID, nil
 }
 
+// findChildren takes a confluence.Pages object and iterates through it to find child pages of the provided
+// parentPageID, then calls itself to find children of those children and so on.
+func findChildren(pages confluence.Pages, parentPageID string) confluence.Pages {
+	var children confluence.Pages
+
+	for _, page := range pages.Results {
+		if page.ParentID == parentPageID {
+			children.Results = append(children.Results, page)
+		}
+	}
+
+	if len(children.Results) > 0 {
+		for _, page := range children.Results {
+			next := findChildren(pages, page.ID)
+			children.Results = append(children.Results, next.Results...)
+		}
+	}
+	return children
+}
+
+func findNotUpdated(pages confluence.Pages, duration time.Duration) (*confluence.Pages, error) {
+	var output confluence.Pages
+	timeLayout := "2006-01-02T15:04:05.000Z"
+	oldestAllowedTime := time.Now().Add(-duration)
+
+	for _, page := range pages.Results {
+		lastUpdatedTime, err := time.Parse(timeLayout, page.Version.CreatedAt)
+		if err != nil {
+			return &confluence.Pages{}, nil
+		}
+		if lastUpdatedTime.Before(oldestAllowedTime) {
+			output.Results = append(output.Results, page)
+		}
+	}
+	return &output, nil
+}
+
 // Run gets metadata for all the pages in a Confluence space and then filters out the pages that 1) are not descendents
 // of the provided parent page and 2) have been updated within the specified time period. Doing it this way (rather than
 // using the Children API endpoint) dramatically reduces the number of API calls and the total time taken.
@@ -169,7 +105,7 @@ func Run(ctx context.Context, jobName string, cfg Config) ([]common.CollectedDat
 	}
 
 	// Get all pages in the space
-	pages, _, err := common.ConfluenceClient.Page.GetPagesInSpace(ctx, spaceID, &confluence.GetPagesInSpaceOptions{
+	pagesInSpace, _, err := common.ConfluenceClient.Page.GetPagesInSpace(ctx, spaceID, &confluence.GetPagesInSpaceOptions{
 		Limit:                 cfg.ResultsLimit,
 		SerializeIDsAsStrings: true,
 	})
@@ -178,16 +114,37 @@ func Run(ctx context.Context, jobName string, cfg Config) ([]common.CollectedDat
 	}
 
 	// Filter the list so it only includes children of the parent page ID provided in the config
-	filtered := findChildren(*pages, cfg.ParentPageID)
-	PromLastUpdatePagesTotal.WithLabelValues(jobName).Set(float64(len(filtered.Results)))
-	fmt.Print(filtered)
+	children := findChildren(*pagesInSpace, cfg.ParentPageID)
+	PromLastUpdatePagesTotal.WithLabelValues(jobName).Set(float64(len(children.Results)))
 
 	// Find pages not updated since the provided duration
-	// duration, err := time.ParseDuration(cfg.Duration)
-	// if err != nil {
-	// 	common.PromErrors.WithLabelValues(pkg).Inc()
-	// 	log.Printf("%s: couldn't parse duration %s: %s", jobName, cfg.Duration, err)
-	// 	return nil, err
-	// }
+	duration, err := time.ParseDuration(cfg.Duration)
+	if err != nil {
+		common.PromErrors.WithLabelValues(pkg).Inc()
+		log.Printf("%s: couldn't parse duration %s: %s", jobName, cfg.Duration, err)
+		return nil, err
+	}
+	notUpdated, err := findNotUpdated(children, duration)
+	if err != nil {
+		common.PromErrors.WithLabelValues(pkg).Inc()
+		log.Printf("%s: couldn't parse duration %s: %s", jobName, cfg.Duration, err)
+		return nil, err
+	}
+	log.Printf("%s: %+v pages found older than %s", jobName, len(notUpdated.Results), cfg.Duration)
+	sort.SliceStable(notUpdated.Results, func(i, j int) bool {
+		return notUpdated.Results[i].Version.CreatedAt > notUpdated.Results[j].Version.CreatedAt
+	})
+
+	baseURL := common.ConfluenceClient.BaseURL
+	for _, page := range notUpdated.Results {
+		pageURL := path.Join(baseURL.String(), fmt.Sprintf("/wiki/pages/viewpage.action?pageId=%s", page.ID))
+		collectedData = append(collectedData,
+			common.CollectedData{
+				Summary:     fmt.Sprintf("\"%s\" has not been updated since %s", page.Title, page.Version.CreatedAt),
+				Description: fmt.Sprintf("Page ID: %s\nPage Title: %s\nLast Updated: %s\nURL: %s\n\nPlease review this page and update any out-of-date information if required.\n\n", page.ID, page.Title, page.Version.CreatedAt, pageURL),
+			},
+		)
+	}
+
 	return collectedData, nil
 }
